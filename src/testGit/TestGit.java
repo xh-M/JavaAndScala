32e4a97c6f5dd767b16fafcb56582c93c6cc148e
mxh <mxh991122@aliyun.com>
2021-01-12 02:17:59 +0800

    testGit包下创建TestGit类

---
+++ src/testGit/TestGit.java
@@ -0,0 +1,11 @@
+package testGit;
+
+/**
+ * @Auther: XianghuiMeng
+ * @Date: 2021/1/12
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+}
